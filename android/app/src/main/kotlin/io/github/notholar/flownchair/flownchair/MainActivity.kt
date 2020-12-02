bb42825bc7e26a50fbc533fb04c9fc6fb078f691
Holar <vitalii.hurianov@gmail.com>
2020-12-02 23:24:51 +0200

    Make the app background transparent

--- android/app/src/main/kotlin/io/github/notholar/flownchair/flownchair/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/notholar/flownchair/flownchair/MainActivity.kt
@@ -1,6 +1,19 @@
 package io.github.notholar.flownchair.flownchair
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivityLaunchConfigs.BackgroundMode.transparent
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", transparent.toString())
+        super.onCreate(savedInstanceState)
+    }
 }
