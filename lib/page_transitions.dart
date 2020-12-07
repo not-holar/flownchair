e96933c3460d959e09b5c9c76771ac7a2df69908
Holar <vitalii.hurianov@gmail.com>
2020-12-07 05:50:53 +0200

    Fine-tune page animations

--- lib/page_transitions.dart
+++ lib/page_transitions.dart
@@ -1,4 +1,4 @@
-import 'package:animations/animations.dart' as animations;
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class CustomPageTransitionsBuilder extends PageTransitionsBuilder {
@@ -12,12 +12,196 @@ class CustomPageTransitionsBuilder extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    return animations.SharedAxisTransition(
+    return SharedAxisTransition(
       animation: animation,
       secondaryAnimation: secondaryAnimation,
-      transitionType: animations.SharedAxisTransitionType.vertical,
-      fillColor: Colors.transparent,
       child: child,
     );
   }
+}
+
+// Copied from animations 1.1.2 and edited
+
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+class SharedAxisTransition extends StatelessWidget {
+  /// Creates a [SharedAxisTransition].
+  ///
+  /// The [animation] and [secondaryAnimation] argument are required and must
+  /// not be null.
+  const SharedAxisTransition({
+    Key? key,
+    required this.animation,
+    required this.secondaryAnimation,
+    required this.child,
+  }) : super(key: key);
+
+  /// The animation that drives the [child]'s entrance and exit.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.animate], which is the value given to this property
+  ///    when it is used as a page transition.
+  final Animation<double> animation;
+
+  /// The animation that transitions [child] when new content is pushed on top
+  /// of it.
+  ///
+  /// See also:
+  ///
+  ///  * [TransitionRoute.secondaryAnimation], which is the value given to this
+  ///    property when the it is used as a page transition.
+  final Animation<double> secondaryAnimation;
+
+  /// The widget below this widget in the tree.
+  ///
+  /// This widget will transition in and out as driven by [animation] and
+  /// [secondaryAnimation].
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return DualTransitionBuilder(
+      animation: animation,
+      forwardBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child,
+      ) {
+        return _EnterTransition(
+          animation: animation,
+          child: child,
+        );
+      },
+      reverseBuilder: (
+        BuildContext context,
+        Animation<double> animation,
+        Widget? child,
+      ) {
+        return _ExitTransition(
+          animation: animation,
+          reverse: true,
+          child: child,
+        );
+      },
+      child: DualTransitionBuilder(
+        animation: ReverseAnimation(secondaryAnimation),
+        forwardBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Widget? child,
+        ) {
+          return _EnterTransition(
+            animation: animation,
+            reverse: true,
+            child: child,
+          );
+        },
+        reverseBuilder: (
+          BuildContext context,
+          Animation<double> animation,
+          Widget? child,
+        ) {
+          return _ExitTransition(
+            animation: animation,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+class _EnterTransition extends StatelessWidget {
+  const _EnterTransition({
+    required this.animation,
+    this.reverse = false,
+    required this.child,
+  });
+
+  final Animation<double> animation;
+  final Widget? child;
+  final bool reverse;
+
+  static final Animatable<double> _fadeInTransition = CurveTween(
+    curve: decelerateEasing,
+  ).chain(CurveTween(curve: const Interval(0.3, 1.0)));
+
+  @override
+  Widget build(BuildContext context) {
+    final Animatable<Offset> slideInTransition = Tween<Offset>(
+      begin: Offset(0.0, !reverse ? 30.0 : -30.0),
+      end: Offset.zero,
+    ).chain(CurveTween(curve: standardEasing));
+
+    return FadeTransition(
+      opacity: _fadeInTransition.animate(animation),
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          return Transform.translate(
+            offset: slideInTransition.evaluate(animation),
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+class _ExitTransition extends StatelessWidget {
+  const _ExitTransition({
+    required this.animation,
+    this.reverse = false,
+    required this.child,
+  });
+
+  final Animation<double> animation;
+  final bool reverse;
+  final Widget? child;
+
+  static final Animatable<double> _fadeOutTransition = _FlippedCurveTween(
+    curve: decelerateEasing,
+  ).chain(CurveTween(curve: const Interval(0.0, 0.45)));
+
+  @override
+  Widget build(BuildContext context) {
+    final Animatable<Offset> slideOutTransition = Tween<Offset>(
+      begin: Offset.zero,
+      end: Offset(0.0, !reverse ? -30.0 : 100.0),
+    ).chain(CurveTween(curve: accelerateEasing));
+
+    return FadeTransition(
+      opacity: _fadeOutTransition.animate(animation),
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          return Transform.translate(
+            offset: slideOutTransition.evaluate(animation),
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
+  }
+}
+
+/// Enables creating a flipped [CurveTween].
+///
+/// This creates a [CurveTween] that evaluates to a result that flips the
+/// tween vertically.
+///
+/// This tween sequence assumes that the evaluated result has to be a double
+/// between 0.0 and 1.0.
+class _FlippedCurveTween extends CurveTween {
+  /// Creates a vertically flipped [CurveTween].
+  _FlippedCurveTween({required Curve curve}) : super(curve: curve);
+
+  @override
+  double transform(double t) => 1.0 - super.transform(t);
 }
