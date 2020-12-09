16be8977044c6bdce679315c2b6ee655ca0094dc
Holar <vitalii.hurianov@gmail.com>
2020-12-09 23:35:28 +0200

    Revamp Overscroller

--- lib/widgets/overscroller.dart
+++ lib/widgets/overscroller.dart
@@ -1,103 +1,45 @@
 import 'package:flutter/material.dart';
 
+typedef OverscrollerTestFunction = bool Function(ScrollMetrics metrics);
+
 class Overscroller extends StatefulWidget {
   const Overscroller({
     Key? key,
-    this.controller,
     required this.test,
-    required this.onOverscrolled,
+    required this.onOverscroll,
     required this.child,
   }) : super(key: key);
 
-  final bool Function(double offset) test;
-  final void Function() onOverscrolled;
+  final OverscrollerTestFunction test;
+  final void Function() onOverscroll;
   final Widget child;
-  final ScrollController? controller;
 
   @override
   _OverscrollerState createState() => _OverscrollerState();
 }
 
 class _OverscrollerState extends State<Overscroller> {
-  late final ScrollController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = widget.controller ?? ScrollController();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return OverscrollerBase(
-      key: const Key("Base"),
-      controller: _controller,
-      test: widget.test,
-      onOverscrolled: widget.onOverscrolled,
-      child: CustomScrollView(
-        controller: _controller,
-        physics: const BouncingScrollPhysics(
-          parent: AlwaysScrollableScrollPhysics(),
-        ),
-        clipBehavior: Clip.none,
-        slivers: [
-          SliverFillRemaining(child: widget.child),
-        ],
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    if (widget.controller == null) _controller.dispose();
-
-    super.dispose();
-  }
-}
-
-/// Overscroller's base widget, can be used individually with other scrollviews
-class OverscrollerBase extends StatefulWidget {
-  const OverscrollerBase({
-    Key? key,
-    required this.controller,
-    required this.test,
-    required this.onOverscrolled,
-    required this.child,
-  }) : super(key: key);
-
-  final bool Function(double offset) test;
-  final void Function() onOverscrolled;
-  final Widget child;
-  final ScrollController controller;
-
-  @override
-  _OverscrollerBaseState createState() => _OverscrollerBaseState();
-}
-
-class _OverscrollerBaseState extends State<OverscrollerBase> {
   /// Whether the previous position of the scroll controller passed the
   /// activation test, it's needed so that [widget.onOverscrolled] is only called
   /// once per pass
   bool _previouslyPassed = false;
 
   @override
-  void initState() {
-    super.initState();
-
-    widget.controller.addListener(() {
-      final passed = widget.test(widget.controller.offset);
-
-      if (passed != _previouslyPassed) {
-        if (passed) widget.onOverscrolled();
-
-        _previouslyPassed = passed;
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return NotificationListener(
+      onNotification: (notification) {
+        if (notification is ScrollNotification) {
+          final passed = widget.test(notification.metrics);
+
+          if (passed != _previouslyPassed) {
+            if (passed) widget.onOverscroll();
+
+            _previouslyPassed = passed;
+          }
+        }
+        return false;
+      },
+      child: widget.child,
+    );
   }
 }
